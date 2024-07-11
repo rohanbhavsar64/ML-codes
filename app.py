8a2757f3069cdb48085c2caf7c0d58de65502517
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-11 11:22:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,134 +154,69 @@ elif h=='PREDICATION':
     l = ['5d','1mo','3mo','1y','5y','max']
     i = st.radio('Period', l,horizontal=True)
     df = yf.download(ticker, period=i)
-    def LSTM_ALGO(df):
-        #Split data into training set and test set
-        dataset_train=df.iloc[0:int(0.8*len(df)),:]
-        dataset_test=df.iloc[int(0.8*len(df)):,:]
-        ############# NOTE #################
-        #TO PREDICT STOCK PRICES OF NEXT N DAYS, STORE PREVIOUS N DAYS IN MEMORY WHILE TRAINING
-        # HERE N=7
-        ###dataset_train=pd.read_csv('Google_Stock_Price_Train.csv')
-        training_set=df.iloc[:,4:5].values# 1:2, to store as numpy array else Series obj will be stored
-        #select cols using above manner to select as float64 type, view in var explorer
+    def LIN_REG_ALGO(df):
+        #No of days to be forcasted in future
+        forecast_out = int(7)
+        #Price after n days
+        df['Close after n days'] = df['Close'].shift(-forecast_out)
+        #New df with only relevant data
+        df_new=df[['Close','Close after n days']]
 
-        #Feature Scaling
-        from sklearn.preprocessing import MinMaxScaler
-        sc=MinMaxScaler(feature_range=(0,1))#Scaled values btween 0,1
-        training_set_scaled=sc.fit_transform(training_set)
-        #In scaling, fit_transform for training, transform for test
+        #Structure data for train, test & forecast
+        #lables of known data, discard last 35 rows
+        y =np.array(df_new.iloc[:-forecast_out,-1])
+        y=np.reshape(y, (-1,1))
+        #all cols of known data except lables, discard last 35 rows
+        X=np.array(df_new.iloc[:-forecast_out,0:-1])
+        #Unknown, X to be forecasted
+        X_to_be_forecasted=np.array(df_new.iloc[-forecast_out:,0:-1])
         
-        #Creating data stucture with 7 timesteps and 1 output. 
-        #7 timesteps meaning storing trends from 7 days before current day to predict 1 next output
-        X_train=[]#memory with 7 days from day i
-        y_train=[]#day i
-        for i in range(7,len(training_set_scaled)):
-            X_train.append(training_set_scaled[i-7:i,0])
-            y_train.append(training_set_scaled[i,0])
-        #Convert list to numpy arrays
-        X_train=np.array(X_train)
-        y_train=np.array(y_train)
-        X_forecast=np.array(X_train[-1,1:])
-        X_forecast=np.append(X_forecast,y_train[-1])
-        #Reshaping: Adding 3rd dimension
-        X_train=np.reshape(X_train, (X_train.shape[0],X_train.shape[1],1))#.shape 0=row,1=col
-        X_forecast=np.reshape(X_forecast, (1,X_forecast.shape[0],1))
-        #For X_train=np.reshape(no. of rows/samples, timesteps, no. of cols/features)
+        #Traning, testing to plot graphs, check accuracy
+        X_train=X[0:int(0.8*len(df)),:]
+        X_test=X[int(0.8*len(df)):,:]
+        y_train=y[0:int(0.8*len(df)),:]
+        y_test=y[int(0.8*len(df)):,:]
         
-        #Building RNN
-        from keras.models import Sequential
-        from keras.layers import Dense
-        from keras.layers import Dropout
-        from keras.layers import LSTM
+        # Feature Scaling===Normalization
+        from sklearn.preprocessing import StandardScaler
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.transform(X_test)
         
-        #Initialise RNN
-        regressor=Sequential()
-        
-        #Add first LSTM layer
-        regressor.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1],1)))
-        #units=no. of neurons in layer
-        #input_shape=(timesteps,no. of cols/features)
-        #return_seq=True for sending recc memory. For last layer, retrun_seq=False since end of the line
-        regressor.add(Dropout(0.1))
-        
-        #Add 2nd LSTM layer
-        regressor.add(LSTM(units=50,return_sequences=True))
-        regressor.add(Dropout(0.1))
-        
-        #Add 3rd LSTM layer
-        regressor.add(LSTM(units=50,return_sequences=True))
-        regressor.add(Dropout(0.1))
-        
-        #Add 4th LSTM layer
-        regressor.add(LSTM(units=50))
-        regressor.add(Dropout(0.1))
-        
-        #Add o/p layer
-        regressor.add(Dense(units=1))
-        
-        #Compile
-        regressor.compile(optimizer='adam',loss='mean_squared_error')
+        X_to_be_forecasted=sc.transform(X_to_be_forecasted)
         
         #Training
-        regressor.fit(X_train,y_train,epochs=25,batch_size=32 )
-        #For lstm, batch_size=power of 2
+        clf = LinearRegression(n_jobs=-1)
+        clf.fit(X_train, y_train)
         
         #Testing
-        ###dataset_test=pd.read_csv('Google_Stock_Price_Test.csv')
-        real_stock_price=dataset_test.iloc[:,4:5].values
+        y_test_pred=clf.predict(X_test)
+        y_test_pred=y_test_pred*(1.04)
+        import matplotlib.pyplot as plt2
+        fig = plt2.figure(figsize=(7.2,4.8),dpi=65)
+        plt2.plot(y_test,label='Actual Price' )
+        plt2.plot(y_test_pred,label='Predicted Price')
         
-        #To predict, we need stock prices of 7 days before the test set
-        #So combine train and test set to get the entire data set
-        dataset_total=pd.concat((dataset_train['Close'],dataset_test['Close']),axis=0) 
-        testing_set=dataset_total[ len(dataset_total) -len(dataset_test) -7: ].values
-        testing_set=testing_set.reshape(-1,1)
-        #-1=till last row, (-1,1)=>(80,1). otherwise only (80,0)
+        plt2.legend(loc=4)
+        plt2.savefig('static/LR.png')
+        plt2.close(fig)
         
-        #Feature scaling
-        testing_set=sc.transform(testing_set)
-        
-        #Create data structure
-        X_test=[]
-        for i in range(7,len(testing_set)):
-            X_test.append(testing_set[i-7:i,0])
-            #Convert list to numpy arrays
-        X_test=np.array(X_test)
-        
-        #Reshaping: Adding 3rd dimension
-        X_test=np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
-        
-        #Testing Prediction
-        predicted_stock_price=regressor.predict(X_test)
-        
-        #Getting original prices back from scaled values
-        predicted_stock_price=sc.inverse_transform(predicted_stock_price)
-        fig = plt.figure(figsize=(7.2,4.8),dpi=65)
-        plt.plot(real_stock_price,label='Actual Price')  
-        plt.plot(predicted_stock_price,label='Predicted Price')
-          
-        plt.legend(loc=4)
-        plt.savefig('static/LSTM.png')
-        plt.close(fig)
+        error_lr = math.sqrt(mean_squared_error(y_test, y_test_pred))
         
         
-        error_lstm = math.sqrt(mean_squared_error(real_stock_price, predicted_stock_price))
-        
-        
-        #Forecasting Prediction
-        forecasted_stock_price=regressor.predict(X_forecast)
-        
-        #Getting original prices back from scaled values
-        forecasted_stock_price=sc.inverse_transform(forecasted_stock_price)
-        
-        lstm_pred=forecasted_stock_price[0,0]
+        #Forecasting
+        forecast_set = clf.predict(X_to_be_forecasted)
+        forecast_set=forecast_set*(1.04)
+        mean=forecast_set.mean()
+        lr_pred=forecast_set[0,0]
         print()
         print("##############################################################################")
-        print("Tomorrow's ",quote," Closing Price Prediction by LSTM: ",lstm_pred)
-        print("LSTM RMSE:",error_lstm)
+        print("Tomorrow's ",quote," Closing Price Prediction by Linear Regression: ",lr_pred)
+        print("Linear Regression RMSE:",error_lr)
         print("##############################################################################")
-        return lstm_pred,error_lstm
+        return df, lr_pred, forecast_set, mean, error_lr
 
-    st.write(LSTM_ALGO(df))
+    st.write(LIN_REG_ALGO(df))
 else:
     st.subheader('Recommendations')
     MMM=yf.Ticker(ticker)
