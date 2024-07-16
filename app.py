c2c47d7489bae6230c88f10fdb3a1dbe8967d1e8
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-16 19:15:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import plotly.express as px
 import streamlit as st
 st.title("Stock Analysis")
-fd=pd.read_csvconstituents.csv')
+fd=pd.read_csv('constituents.csv')
 f=fd['Symbol'].unique()
 #Mt(15px) Lh(1.6)
 ticker=st.selectbox("Company",f)
