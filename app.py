b6915901376db48bd55f47be344cff6f158e5591
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-11-20 19:59:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -148,11 +148,85 @@ elif h=='Financial':
         data=[[a1,b1],[a2,b2],[a3,b3],[a4,b4]]
         df=pd.DataFrame(data,columns=['Year',m])
         st.write(px.bar(df,x='Year',y=m,color_discrete_sequence=['orange']))
-elif h=='Historical Data':
-    l = ['5d','1mo','3mo','1y','5y','max']
-    i = st.radio('Period', l,horizontal=True)
-    df = yf.download(ticker, period=i)
-    st.write(df)
+elif h == 'Historical Data':
+    START = "2015-01-01"
+    TODAY = date.today().strftime("%Y-%m-%d")
+
+    def load_data(ticker):
+        data = yf.download(ticker, START, TODAY)
+        data.reset_index(inplace=True)
+        return data
+
+    data = load_data('AAPL')
+    df = data.drop(['Date', 'Adj Close'], axis=1)
+
+    # Splitting the data into training and testing sets
+    train = pd.DataFrame(data[0:int(len(data) * 0.70)])
+    test = pd.DataFrame(data[int(len(data) * 0.70): int(len(data))])
+
+    # Scaling the data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    train_close = train.iloc[:, 4:5].values
+    test_close = test.iloc[:, 4:5].values
+    data_training_array = scaler.fit_transform(train_close)
+
+    x_train = []
+    y_train = []
+
+    for i in range(100, data_training_array.shape[0]):
+        x_train.append(data_training_array[i-100:i])
+        y_train.append(data_training_array[i, 0])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    # Building the LSTM model
+    model = Sequential()
+    model.add(LSTM(units=50, activation='relu', return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(units=60, activation='relu', return_sequences=True))
+    model.add(Dropout(0.3))
+
+    model.add(LSTM(units=80, activation='relu', return_sequences=True))
+    model.add(Dropout(0.4))
+
+    model.add(LSTM(units=120, activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(units=1))
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['MAE'])
+    model.fit(x_train, y_train, epochs=10)
+    model.save('keras_model.h5')
+
+    # Preparing the test data
+    past_100_days = pd.DataFrame(train_close[-100:])
+    test_df = pd.DataFrame(test_close)
+    final_df = pd.concat([past_100_days, test_df], ignore_index=True)
+    input_data = scaler.transform(final_df)  # Use transform instead of fit_transform
+
+    x_test = []
+    y_test = []
+    for i in range(100, input_data.shape[0]):
+        x_test.append(input_data[i-100:i])
+        y_test.append(input_data[i, 0])
+
+    x_test, y_test = np.array(x_test), np.array(y_test)
+
+    # Making predictions
+    y_pred = model.predict(x_test)
+    scale_factor = 1 / scaler.scale_[0]  # Correct scaling factor
+    y_pred = y_pred * scale_factor
+    y_test = y_test * scale_factor
+
+    # Plotting the results
+    plt.figure(figsize=(12, 6))
+    plt.plot(y_test, 'b', label="Original Price")
+    plt.plot(y_pred, 'r', label="Predicted Price")
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.show()
+     
 else:
     st.subheader('Recommendations')
     MMM=yf.Ticker(ticker)
